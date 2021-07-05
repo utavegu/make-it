75a600d38b56e7c394549ca8c043510473de0d9f
Utavegu <utavegu@list.ru>
2021-07-05 11:15:31 +0500

    Поправил бандлер

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,14 @@
 "use strict";
 
-/* ПОДКЛЮЧЕНИЕ */
+var gulp = require("gulp");
+var less = require("gulp-less"); 
+var sourcemap = require("gulp-sourcemaps");
+var server = require("browser-sync").create();
+var plumber = require("gulp-plumber");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var del = require("del");
 
-var gulp = require("gulp");  // сам Галп
-
-// Набор для разработки
-var less = require("gulp-less");  // декодер less-css
-var sourcemap = require("gulp-sourcemaps");  // карта исходников
-var server = require("browser-sync").create();  // локальный веб-сервер
-var plumber = require("gulp-plumber");  // гаситель критичности ошибок (ЛИШНЕЕ, ИМХО)
-
-// Работа с графикой
-var imagemin = require("gulp-imagemin");  // 4 плагина по оптимизации изображений
-var webp = require("gulp-webp");  // преобразователь растра в webp
-/*
-var svgstore = require("gulp-svgstore");  // сборщик svg-спрайта
-var posthtml = require("gulp-posthtml");  // post-html
-var include = require("posthtml-include");  // и плагин инклюд для него
-*/
-
-// Набор для деплоя
-var del = require("del");  // галп-удалятель
-var rename = require("gulp-rename");  // галп-переименователь
-
-var autoprefixer = require("gulp-autoprefixer");  // автопрефиксер (ТЫ НАВЕРНОЕ БЕЗ ПОСТ-ЦСС НЕ ФУРЫЧИШЬ)... Хотя там другой был... старый... вот лучше попробуй с ним разобраться. Или пересмотреть новую лекцию по автоматизации
-
-/*
-var minhtml = require("gulp-minimize");  // минификатор html
-var csso = require("gulp-csso");  // минификатор css
-var minjs = require("gulp-uglify");  // минификатор js
-*/
-
-const ghPages = require('gh-pages');
-const path = require('path');
-
-
-/* ЗАДАЧИ */
-
-// РАЗРАБОТКА
-// Преобразование Less, карта исходников, локальный сервер, гаситель критичности ошибок
 gulp.task("development", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -49,7 +19,6 @@ gulp.task("development", function () {
     .pipe(server.stream());
 });
 
-// Автообновления локального сервера при разработке
 gulp.task("live reload", function () {
   server.init({
     server: "source/",
@@ -62,11 +31,6 @@ gulp.task("live reload", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-
-
-// ГРАФИКА
-
-//Оптимизация изображений (png, jpg, svg)
 gulp.task("images optimization", function() {
 	return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
@@ -77,97 +41,52 @@ gulp.task("images optimization", function() {
     .pipe(gulp.dest("build/img"))
 });
 
-//Создание webp из растра и копирование в продакшн
 gulp.task("create webp", function() {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp())
     .pipe(gulp.dest("build/img"));
 })
 
-// Создание векторного спрайта
-// gulp.task("create vector sprite", function() {
-//   return gulp.src("source/img/svg/**/*.svg")
-//   .pipe(svgstore({
-//     inlineSvg: true
-//   }))
-//   .pipe(rename("sprite.svg"))
-//   .pipe(gulp.dest("source/img/svg"))
-//   .pipe(gulp.dest("build/img/svg"));
-// })
-
-
-
-// ПРЕДЕПЛОЙ
-
-// Удаление папки билда (чтобы удалённое в source при разработке не оставалось в build)
 gulp.task("clean build", function () {
 	return del("build");
 });
 
-// Минимизация CSS и закидывание его в продакшн
 gulp.task("css for production", function () {
-  return gulp.src("source/css/style.css") // Взяли отсюда
-    .pipe(autoprefixer()) // Прогнал через автопрефиксер
-    // .pipe(csso()) // Минифицировали
-    // .pipe(rename("style.css")) // Обозвали вот так
-    .pipe(gulp.dest("build/css")); // Закинули сюда
+  return gulp.src("source/css/style.css")
+    .pipe(gulp.dest("build/css"));
 });
 
-// Минификация JS
 gulp.task("compress js", function() {
   return gulp.src("source/js/*.js")
-  // .pipe(minjs())
   .pipe(gulp.dest("build/js"));
 });
 
-// Минификация HTML (с предварительной вставкой содержимого include туда)
 gulp.task("html for production", function() {
   return gulp.src("source/*.html")
-    /*
-    .pipe(posthtml([
-      include()
-    ]))
-    */
-    // .pipe(minhtml())
     .pipe(gulp.dest("build"));
 })
 
-// Копирование остального в папку продакшена
 gulp.task("copy other", function() {
 	return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/*.{ico,png}",
     "source/video/*.{mp4,webm,ogg}"
-    // "source/video/**/*"
     ], {
 			base: "source"
 		})
 		.pipe(gulp.dest("build"));
 });
 
-// ДЕПЛОЙ
-function deploy(cb) {
-  ghPages.publish(path.join(process.cwd(), './build'), cb);
-}
-exports.deploy = deploy;
-
-
-
-/* КОМАНДЫ */
-
-// Запуск разработки
 gulp.task("start", gulp.series(
   "development",
   "live reload"
 ));
 
-// Сборка проекта
 gulp.task("build", gulp.series(
   "clean build",
   "development",
   "images optimization",
   "create webp",
-  // "create vector sprite",
   "html for production",
   "css for production",
   "compress js",
